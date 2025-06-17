3c8db110780729f870f8b4a399d7102ed5bcf0c6
jmunroob-admin <james.munro@openbrolly.com>
2025-06-17 13:11:58 +0100

    cron.js update

--- src/server.js
+++ src/server.js
@@ -2,9 +2,12 @@ import express from "express";
 import { ENV } from "./config/env.js";
 import { db } from "./config/db.js";
 import { favouriteRecipesTable } from "./db/schema.js";
+import job from "./config/cron.js";
 
 const app = express();
 const PORT = ENV.PORT || 8081;
+
+if (ENV.NODE_ENV === "production") job.start();
 
 app.use(express.json())
 
